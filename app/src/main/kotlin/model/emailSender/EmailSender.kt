b87966daf6ce88a6aef661f3bae459d5e111c512
Stepan Gorlov <Stepagor105@gmail.com>
2025-10-05 00:35:05 +0300

    feat: add send email func

--- app/src/main/kotlin/model/emailSender/EmailSender.kt
+++ app/src/main/kotlin/model/emailSender/EmailSender.kt
@@ -2,6 +2,8 @@ package model.emailSender
 
 import org.apache.commons.mail.EmailAttachment
 import org.apache.commons.mail.MultiPartEmail
+import kotlin.io.path.Path
+import kotlin.io.path.exists
 
 class EmailSender private constructor() {
     private var email: Email? = null
@@ -35,5 +37,42 @@ class EmailSender private constructor() {
         this.email = Email(email)
         this.password = password
         this.smtp = domainToServer[this.email?.getDomain()] ?: throw IllegalArgumentException("Invalid email format")
+    }
+
+    fun send(recipients: List<Email>, subject: String, text: String, filePath: String?) {
+        if (recipients.size > 30) {
+            throw IllegalArgumentException("Too many recipients")
+        }
+
+        val email = MultiPartEmail()
+
+        filePath?.apply {
+            val attach = EmailAttachment()
+            if (!Path(filePath).exists()) {
+                throw IllegalArgumentException("Path does not exist")
+            }
+            attach.path = filePath
+            attach.disposition = EmailAttachment.ATTACHMENT
+            email.attach(attach)
+        }
+
+        email.setHostName(serverList[this.smtp]?.first ?: throw IllegalArgumentException("Mail server doesn't exist"))
+        email.setSmtpPort(
+            serverList[this.smtp]?.second ?: throw IllegalArgumentException("Mail server doesn't exist")
+        )
+        email.setAuthentication(
+            this.email?.getEmail() ?: throw IllegalArgumentException("Email can not be null"),
+            this.password
+        )
+        email.isSSLOnConnect = true
+
+        email.setFrom(this.email?.getEmail() ?: throw IllegalArgumentException("Email can not be null"))
+        recipients.forEach { recipient ->
+            email.addTo(recipient.getEmail())
+        }
+        email.setSubject(subject)
+        email.setMsg(text)
+
+        email.send()
     }
 }
